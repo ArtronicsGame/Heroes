298490df1eb8a0632ed2cf5e6550e1bc068d8b20
mastermobin <mvtikm@gmail.com>
2018-08-22 11:33:20 +0430

    Cafebazaar IAB library and classes and also StringCorrecter added

---
+++ Scripts/CafeBazaar.gd
@@ -0,0 +1,63 @@
+extends Node2D
+
+const PurchaseTemp = preload("utils/Purchase.gd")
+const IabResultTemp = preload("utils/IabResult.gd")
+const SkuDetailsTemp = preload("utils/SkuDetails.gd")
+const InventoryTemp = preload("utils/Inventory.gd")
+
+#onready var cafeBazaarIAB = Engine.get_singleton("CafeBazaarIAB")
+var cafeBazaarIAB
+
+signal on_iab_setup_finished(result)
+signal on_iab_purchase_finished(result, purchase)
+signal on_iab_consume_finished(result, purchase)
+signal on_query_inventory_finished(result, inventory)
+
+func _ready():
+	#cafeBazaarIAB.setCallbackId(get_instance_id())
+	pass
+
+func startSetup(key):
+	cafeBazaarIAB.connect(key)
+
+func onIabSetupFinished(response, message):
+	var result = IabResultTemp.new(response, message)
+	emit_signal("on_iab_setup_finished", result)
+
+func onIabPurchaseFinished(response, message, itemType, json, signature):
+	var result = IabResultTemp.new(response, message)
+	if result.isSuccess():
+		var purchase = PurchaseTemp.new(itemType, json, signature)
+		emit_signal("on_iab_purchase_finished", result, purchase)
+	else:
+		emit_signal("on_iab_purchase_finished", result, "Null")
+
+func onConsumeFinished(response, message, itemType, json, signature):
+	var result = IabResultTemp.new(response, message)
+	if result.isSuccess():
+		var purchase = PurchaseTemp.new(itemType, json, signature)
+		emit_signal("on_iab_consume_finished", result, purchase)
+	else:
+		emit_signal("on_iab_consume_finished", result, "Null")
+
+func onQueryInventoryFinished(response, message, purchaseJson, skusJson):
+	var result = IabResultTemp.new(response, message)
+	if result.isSuccess():
+		var inventory = InventoryTemp.new(purchaseJson, skusJson)
+		emit_signal("on_query_inventory_finished", result, inventory)
+	else:
+		emit_signal("on_query_inventory_finished", result, "Null")
+
+func launchPurchaseFlow(sku, developerPayload):
+	cafeBazaarIAB.purchase(sku, developerPayload)
+
+func consumeAsync(purchase):
+	cafeBazaarIAB.consume(purchase.mItemType, purchase.mOriginalJson, purchase.mSignature)
+
+func queryInventoryAsync(details, skus):
+	var skuString = ""
+	for sku in skus:
+		skuString += sku
+		skuString += ";"
+	skuString = skuString.substr(0, skuString.length() - 1)
+	cafeBazaarIAB.queryInventory(details, skuString)
---
+++ Scripts/utils/Inventory.gd
@@ -0,0 +1,46 @@
+var mSkuMap = {}
+var mPurchaseMap = {}
+
+const PurchaseTemp = preload("Purchase.gd")
+const SkuDetailsTemp = preload("SkuDetails.gd")
+
+func _init(purchaseJson, skuJson):
+	var json_conv = JSON.parse(purchaseJson)
+	if json_conv.error == OK:
+		var purchaseMap = json_conv.result
+		for key in purchaseMap:
+			var array = purchaseMap[key].split(";")
+			var newPurchase = PurchaseTemp.new(array[0], array[1], array[2])
+			mPurchaseMap[key] = newPurchase
+	json_conv = JSON.parse(skuJson)
+	if json_conv.error == OK:
+		var skuMap = json_conv.result
+		for key in skuMap:
+			var array = skuMap[key].split(";")
+			var newSkuDetails = SkuDetailsTemp.new(array[0], array[1])
+			mSkuMap[key] = newSkuDetails
+
+func getSkuDetails(sku):
+	return mSkuMap[sku]
+
+func getPurchase(sku):
+	return mPurchaseMap[sku]
+    
+func hasPurchase(sku):
+	return sku in mPurchaseMap.keys()
+
+func hasDetails(sku):
+	return sku in mSkuMap.keys()
+
+func getAllOwnedSkus(itemType):
+	var result = []
+	for p in mPurchaseMap:
+		if mPurchaseMap[p].mItemType == itemType:
+			result.append(p.mSku)
+	return result
+
+func getAllPurchases():
+	var result = []
+	for p in mPurchaseMap:
+		result.append(mPurchaseMap[p])
+	return result
