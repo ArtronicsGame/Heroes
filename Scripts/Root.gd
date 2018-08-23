3c8385961b254e15aa45bd3ee830a37dc11d85df
hojats7731 <hojats7731@gmail.com>
2018-08-23 17:44:30 +0430

    Sync

--- Scripts/Root.gd
+++ Scripts/Root.gd
@@ -2,18 +2,6 @@ extends Node2D
 
 var slavePlayer = load("res://Scenes/SlavePlayer.tscn")
 
-"""func _on_Logic_change_pos(idn, newPos, rotation):
-	print(idn)
-	var id = str(idn)
-	if has_node(id):
-		var lastPos = get_node(id).position
-		get_node(id).move(newPos - lastPos)
-	else:
-		var newPlayer = slavePlayer.instance()
-		newPlayer.set_name(id)
-		add_child(newPlayer)
-		get_node(id).position = newPos"""
-
 func _ready():
 	set_process(true)
 
