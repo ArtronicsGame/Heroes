758a6896384f9c3329a99974f4875c6af5023f6b
Mobin Vahdati <mvtikm@gmail.com>
2018-08-28 21:14:16 +0430

    Left half for joystick added

--- Scripts/JoyStick.gd
+++ Scripts/JoyStick.gd
@@ -5,6 +5,7 @@ var joyStickIndex = -1
 var startPos = Vector2(0, 0)
 var endPos
 var nulls = false
+var screensize
 
 
 export (int) var SwitchTimeInterval = 500
@@ -23,7 +24,8 @@ signal on_joyStick_changed(moveVector)
 
 func _ready():
 	self.modulate = MoveColor
-	SwitchDistInterval = (get_viewport_rect().size * SwitchDistIntervalRatio).length()
+	screensize = get_viewport_rect().size 
+	SwitchDistInterval = (screensize * SwitchDistIntervalRatio).length()
 	set_process(true)
 	set_process_input(true)
 	visible = false
@@ -35,32 +37,33 @@ func _process(delta):
 
 func _input(event):
 	if event is InputEventScreenTouch:
-		if event.is_pressed():
-			var now = OS.get_ticks_msec()
-			if (now - lastTime) < SwitchTimeInterval:
-				if (lastPos - event.position).length() < SwitchDistInterval:
-					aimingMode = !aimingMode
-					if aimingMode:
-						self.modulate = AimColor
-					else:
-						self.modulate = MoveColor
+		if event.position.x < (screensize.x / 2):
+			if event.is_pressed():
+				var now = OS.get_ticks_msec()
+				if (now - lastTime) < SwitchTimeInterval:
+					if (lastPos - event.position).length() < SwitchDistInterval:
+						aimingMode = !aimingMode
+						if aimingMode:
+							self.modulate = AimColor
+						else:
+							self.modulate = MoveColor
+			lastPos = event.position
+			lastTime = OS.get_ticks_msec()
+			
+			if joyStickIndex == -1 && event.is_pressed():
+				startPos = event.position
+				endPos = event.position
+				joyStickIndex = event.index
+				$JoyStickBack.position = startPos
+				$JoyStickBack/Joystick.position = Vector2(0, 0)
+				visible = true
+			elif joyStickIndex == event.index && !event.is_pressed():
+				movementVector = Vector2(0, 0)
+				visible = false
+				joyStickIndex = -1
+		else:
+			pass #Attack Button
 	
-	if event is InputEventScreenTouch:
-		lastPos = event.position
-		lastTime = OS.get_ticks_msec()
-	
-	if event is InputEventScreenTouch: 
-		if joyStickIndex == -1 && event.is_pressed():
-			startPos = event.position
-			endPos = event.position
-			joyStickIndex = event.index
-			$JoyStickBack.position = startPos
-			$JoyStickBack/Joystick.position = Vector2(0, 0)
-			visible = true
-		elif joyStickIndex == event.index && !event.is_pressed():
-			movementVector = Vector2(0, 0)
-			visible = false
-			joyStickIndex = -1
 	elif event is InputEventScreenDrag:
 		if joyStickIndex == event.index:
 			endPos = event.position
