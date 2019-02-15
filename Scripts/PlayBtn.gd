e3e7715d43fa4e73d600dc84072247355b118bd8
Galiold <ali.goldani@ymail.com>
2019-02-15 11:36:53 +0330

    Test Drive

--- Scripts/PlayBtn.gd
+++ Scripts/PlayBtn.gd
@@ -1,10 +1,16 @@
 extends Button
 
+func _ready():
+	TCPConnection.connect("on_message_received", self, "msg_show")
+
 func _on_PlayBtn_button_down():
 	TCPConnection.Send({
 		"_type" : "PlayerController.new",
-		"_info" : {"username" : "Mobin"}
+		"_info" : {"username" : "Ahmad"}
 	})
+	
+func msg_show(msg):
+	print(msg["_userId"])
 
 
 func _on_TCPConnection_on_message_received(msg):
